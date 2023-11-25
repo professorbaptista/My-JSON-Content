680ad93eb2f0fe7643e5164284c22a7b25b7bfcf
professorbaptista <josbaptista28@yahoo.com.br>
2023-11-25 23:56:11 +0000

    Foi acicionado o javascript

---
+++ meujson.js
@@ -0,0 +1,10 @@
+
+
+const meuObj = {
+    nome: "José Baptista", 
+    idade: "48 Anos", 
+    cidade: "Luanda"
+}
+document.getElementById('id-1').innerHTML = meuObj.nome
+document.getElementById('id-1').innerHTML = meuObj.idade
+document.getElementById('id-1').innerHTML = meuObj.cidade
