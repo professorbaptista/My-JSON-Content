725760a731fda34c45d4676c240757ab5dd72521
professorbaptista <josbaptista28@yahoo.com.br>
2023-11-26 00:26:39 +0000

    correcção

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+
+const objecto = {
+    nome: "José Baptista", 
+    idade: "48 Anos", 
+    cidade: "Luanda"
+}
+document.getElementById('id-1').innerHTML = objecto.nome
+document.getElementById('id-1').innerHTML = ojecto.idade
+document.getElementById('id-1').innerHTML = objecto.cidade
